499137debee8cc2f5ec2f5ec9f6145052bc8f87a
artonix101 <artinab@posteo.de>
2026-09-08 09:54:27 +0200

    fix linter warning and allow uninstallation if service_ensure and enable are true

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,14 @@
 class cloudflared::service {
+  $effective_service_ensure = $cloudflared::package_ensure ? {
+    'absent'  => 'stopped',
+    'present' => $cloudflared::service_ensure,
+  }
+  $effective_service_enable = $cloudflared::package_ensure ? {
+    'absent' => false,
+    default  => $cloudflared::service_enable,
+  }
   service { 'cloudflared':
-    ensure => $cloudflared::package_ensure ? {
-      'absent' => 'stopped',
-      'purged' => 'stopped',
-      default  => 'stopped',
-    },
-    enable => $cloudflared::package_ensure ? {
-      'absent' => false,
-      'purged' => false,
-      default  => false,
-    },
+    ensure => $effective_service_ensure,
+    enable => $effective_service_enable,
   }
 }
